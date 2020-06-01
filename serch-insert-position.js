d7a3af1d4a4d5c375d32bf663472c1d80e2dbbe3
cjmash <jamescollins.jcparty@gmail.com>
2020-06-02 01:33:40 +0300

    search insert position using binary search

--- serch-insert-position.js
+++ serch-insert-position.js
@@ -1,20 +1,41 @@
-// my solution
+// my first solution
+
+// var searchInsert = function(nums, target) {
+//   for(let i = 0; i< nums.length; i++) {
+//       if(nums[i] === target) {
+//           return i
+//       } else if(num[i] > target) {
+//         return i
+//       }  
+//       if(nums.length === (i+1)) {
+//           while(nums[i] > target){
+//             nums[i-1]
+//             i--
+//           } 
+//           return i+=1
+//       } 
+//   }
+// };
+
+
+// binary search  iterrative
 
 var searchInsert = function(nums, target) {
-  for(let i = 0; i< nums.length; i++) {
-      if(nums[i] === target) {
-          return i
-      } else if(num[i] > target) {
-        return i
-      }  
-      if(nums.length === (i+1)) {
-          while(nums[i] > target){
-            nums[i-1]
-            i--
-          } 
-          return i+=1
-      } 
+  let left = 0
+  let right = nums.length -1
+ 
+  while (left <= right) {
+   const mid = Math.floor((left + right) / 2);
+   if(nums[mid] < target) 
+   {
+     left = mid + 1
+   } else if (nums[mid] > target){
+     right = mid -1
+   } else {
+     return mid
+   }
   }
-};
+  return left
+ }
 
-searchInsert([1,3,4,5,6], 2)
+
